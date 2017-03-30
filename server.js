76ee1fcdfe9da825707934c7157897db2f2c7a3c
jesse <jsoldat@hotmail.com>
2017-03-30 08:02:11 +0700

    first graphql request works

--- server.js
+++ server.js
@@ -4,9 +4,30 @@ import express from 'express';
 
 const app = express();
 
-app.use('/graphql', (req, res) => {
-	res.send({data: true});
+import graphqlHTTP from 'express-graphql';
+import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
+
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQuery',
+	description: 'The root query',
+	fields: {
+		viewer: {
+			type: GraphQLString,
+			resolve() {
+				return 'viewer';
+			}
+		}
+	}
 });
+
+const Schema = new GraphQLSchema({
+	query: RootQuery
+})
+
+app.use('/graphql', graphqlHTTP({schema: Schema, graphiql: true}));
+// app.use('/graphql', (req, res) => {
+// 	res.send({data: true});
+// });
 
 app.listen(3000, function(){
 	console.log({running: true});
